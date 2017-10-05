657470d86b7498afaae33898a36f46e94ad6bef6
Monica Gil <mgil@psl.com.co>
2017-10-05 17:22:04 -0500

    CSS Selector

--- protractor/config.ts
+++ protractor/config.ts
@@ -3,9 +3,10 @@ import { reporter } from './helpers/reporter';
 
 export const config: Config = {
   framework: 'jasmine',
-  specs: ['../test/Google.spec.js'],
+  specs: ['../test/**/*.spec.js'],
   SELENIUM_PROMISE_MANAGER: false,
   noGlobals: true,
+  getPageTimeout: 1000,
   onPrepare: () => {
     reporter();
     browser.ignoreSynchronization = true;
