679629d3641be69255342f22649ae7ecf2604210
Monica Gil <mgil@psl.com.co>
2017-10-06 15:25:27 -0500

    se hacen correcciones

--- src/page/OrderResume.page.ts
+++ src/page/OrderResume.page.ts
@@ -1,11 +1,11 @@
 import { $, ElementFinder, promise } from 'protractor';
 
 export class OrderResumePage {
-    private get selectbutton(): ElementFinder {
+    private get selectButton(): ElementFinder {
         return $('#center_column > ul > li > div > div.left-block > div > a.product_img_link > img');
     }
 
     public goToSelect(): promise.Promise<void> {
-        return this.selectbutton.click();
+        return this.selectButton.click();
     }
 }
