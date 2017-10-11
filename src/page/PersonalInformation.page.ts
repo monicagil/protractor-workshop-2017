dc491b63bab94b174eb1adf0f0c6f92b14d6bfc7
monicagil <32166699+monicagil@users.noreply.github.com>
2017-10-11 06:22:58 -0500

    Personalinf test (#19)
    * Más Locators
    
    * Más Locators
    
    * correcciones
    
    * mas correcciones
    
    * correciones const por let y textos en ingles

---
+++ src/page/PersonalInformation.page.ts
@@ -0,0 +1,72 @@
+import { element, by, ElementFinder } from 'protractor';
+
+interface PersonalInformation {
+    firstName: string;
+    lastName?: string;
+    sex?: string;
+    experience?: number;
+    profession?: string[];
+    tools?: string[];
+    continent?: string;
+    commands?: string[];
+}
+
+export class PersonalInformationPage {
+    private get firstNameField(): ElementFinder {
+        return element(by.name('firstname'));
+    }
+
+    private get lastNameField(): ElementFinder {
+        return element(by.name('lastname'));
+    }
+
+    private sexOption(name: string): ElementFinder {
+        return element(by.css(`[name="sex"][value="${name}"]`));
+    }
+
+    private experienceOption(years: number): ElementFinder {
+        return element(by.css(`[name="exp"][value="${years}"]`));
+    }
+
+    private professionCheck(profession: string): ElementFinder {
+        return element(by.css(`[name="profession"][value="${profession}"]`));
+    }
+
+    private toolsCheck(tool: string): ElementFinder {
+        return element(by.css(`[name="tool"][value="${tool}"]`));
+    }
+
+    private continentOption(name: string): ElementFinder {
+        return element(by.id('continents')).element(by.cssContainingText('option', name));
+    }
+
+    private commandsOption(name: string): ElementFinder {
+        return element(by.id('selenium_commands')).element(by.cssContainingText('option', name));
+    }
+
+    private get submitButton(): ElementFinder {
+        return element(by.id('submit'));
+    }
+
+    public async getPageTitle(): Promise<string> {
+        return element(by.id('content')).element(by.tagName('h1')).getText();
+    }
+
+    public async fillForm(form: PersonalInformation): Promise<void> {
+        await this.firstNameField.sendKeys(form.firstName);
+        await this.lastNameField.sendKeys(form.lastName);
+        await this.sexOption(form.sex).click();
+        await this.experienceOption(form.experience).click();
+        for (const profession of form.profession) {
+            await this.professionCheck(profession).click();
+        }
+        for (const tool of form.tools) {
+            await this.toolsCheck(tool).click();
+        }
+        await this.continentOption(form.continent).click();
+        for (const command of form.commands) {
+            await this.commandsOption(command).click();
+        }
+        await this.submitButton.click();
+    }
+}
---
+++ test/Locators.spec.ts
@@ -0,0 +1,30 @@
+import { browser } from 'protractor';
+import { PersonalInformationPage } from '../src/page';
+
+describe('Open page', () => {
+    beforeAll(async () => {
+        await browser.get('http://toolsqa.com/automation-practice-form/');
+    });
+    const personalInformationPage: PersonalInformationPage = new PersonalInformationPage();
+    beforeAll(async () => {
+        await personalInformationPage.fillForm({
+            firstName: 'Alejandro',
+            lastName: 'Perdomo',
+            sex: 'Male',
+            experience: 7,
+            profession: ['Automation Tester'],
+            tools: ['Selenium Webdriver'],
+            continent: 'South America',
+            commands: [
+                'Browser Commands',
+                'Navigation Commands',
+                'Switch Commands',
+                'Wait Commands',
+                'WebElement Commands']
+        });
+    });
+
+    it('the form is ready....', async () => {
+        expect(await personalInformationPage.getPageTitle()).toBe('Practice Automation Form');
+    });
+});
